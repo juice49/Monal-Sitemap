f1ee346f7e0beef8005d6c387bdbcdee3e10bc9c
Ash <ashley@juice49.com>
2014-06-30 10:27:14 +0100

    Initial

---
+++ src/Ash/MonalSitemap/MonalSitemapServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Ash\MonalSitemap;
+
+use Illuminate\Support\ServiceProvider;
+
+class MonalSitemapServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('ash/monal-sitemap');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
